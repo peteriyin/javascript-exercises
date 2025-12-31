8788fa476792372772387a819d5be67b60708f3a
Peter <poderinde619@gmail.com>
2025-12-31 13:45:57 +0100

    14 - 16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (fib) {
+    if (fib < 0) return "OOPS"
+    if (fib == 0) return 0;
+    if (fib == 1) return 1;
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= (fib); i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    };
+    return b;
 
 };
 
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.reduce((acc, curr) => {
+        if (((curr.yearOfDeath || new Date().getFullYear()) - curr.yearOfBirth) > ((acc.yearOfDeath || new Date().getFullYear()) - acc.yearOfBirth)) {
+            return curr;
+        };
+        return acc;
+    })
 };
 
 // Do not edit below this line
